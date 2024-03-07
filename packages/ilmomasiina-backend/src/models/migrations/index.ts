d999a6c0c16e6b195ea023fb23d856e44e6016ae
Petrus Asikainen <petrus.asikainen@aalto.fi>
2024-03-07 18:11:44 +0200

    Add basic database indexing
    Co-Authored-By: Ahlström Kalle <kalle.ahlstrom@iki.fi>

--- packages/ilmomasiina-backend/src/models/migrations/index.ts
+++ packages/ilmomasiina-backend/src/models/migrations/index.ts
@@ -6,6 +6,7 @@ import _0001_add_audit_logs from './0001-add-audit-logs';
 import _0002_add_event_endDate from './0002-add-event-endDate';
 import _0003_add_signup_language from './0003-add-signup-language';
 import _0004_answers_to_json from './0004-answers-to-json';
+import _0005_add_indexes from './0005-add-indexes';
 
 const migrations: RunnableMigration<Sequelize>[] = [
   _0000_initial,
@@ -13,6 +14,7 @@ const migrations: RunnableMigration<Sequelize>[] = [
   _0002_add_event_endDate,
   _0003_add_signup_language,
   _0004_answers_to_json,
+  _0005_add_indexes,
 ];
 
 export default migrations;
